a8fa3898e31b76f89ed61d4875cda10ee63ce142
CWPranayyy <guravpranay81@gmail.com>
2025-06-25 12:16:02 +0530

    .

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import {LiveKitRoom ,VideoConference} from "@livekit/components-react";
 import "@livekit/components-styles";
 import { Channel } from "@prisma/client";
 import { useUser } from "@clerk/nextjs";
@@ -10,39 +10,39 @@ import { set } from "date-fns";
 
 interface MediaRoomProps {
     chatId: string;
-    video: boolean;
-    audio: boolean;
+    video:boolean;
+    audio:boolean;
 
 };
 
-export const MediaRoom = ({
+export const MediaRoom=({
     chatId,
     video,
     audio
-}: MediaRoomProps) => {
-    const { user } = useUser();
+}:MediaRoomProps)=>{
+    const {user} = useUser();
     const [token, setToken] = useState("");
 
     useEffect(() => {
         if (!user?.firstName || !user?.lastName) {
             return;
-
+            
         }
-        const name = `${user.firstName} ${user.lastName}`;
-        (async () => {
+        const name =`${user.firstName} ${user.lastName}`;
+        (async()=>{
             try {
-                const resp = await fetch(`/api/livekit?room=${chatId}&username=${name}`)
-                const data = await resp.json();
+                const resp=await fetch(`/api/livekit?room=${chatId}&username=${name}`)
+                const data =await resp.json();
                 setToken(data.token);
-
+                
             } catch (error) {
                 console.log(error);
             }
         })()
-    }, [user?.firstName, user?.lastName, chatId]);
+    },[user?.firstName, user?.lastName, chatId]);
 
-    if (token === "") {
-        return (
+    if(token === ""){
+        return(
             <div className="flex flex-col items-center justify-center ">
                 <Loader2 className="w-7 h-7 animate-spin text-zinc-500 my-4" />
                 <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading...</p>
@@ -52,18 +52,16 @@ export const MediaRoom = ({
 
     return (
         <LiveKitRoom
-            data-tk-theme="default"
-            serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+        data-tk-theme="default"
+        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
             token={token}
             connect={true}
             video={video}
             audio={audio}
-
-
-        >
-            <div className="w-full h-[calc(100vh-60px)] flex items-center justify-center bg-black overflow-hidden">
-                <VideoConference />
-            </div>
+           
+          
+            >
+            <VideoConference  />
         </LiveKitRoom>
     )
 
